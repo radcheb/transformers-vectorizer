ef7cfa930f0d323139e28c2a6098d99cd9ce3f1c
Radhwane Chebaane <radcheb@gmail.com>
2019-12-22 18:49:36 +0100

    fix cammembert vectorizer tests

--- tests/test_cammembert_vectorizer.py
+++ tests/test_cammembert_vectorizer.py
@@ -7,4 +7,4 @@ def test_camembert_vectors():
     text = "J'aime le camembert!"
     vectors = camembert_vectorizer.to_vectors([text])
     assert len(vectors) == 1
-    assert vectors[0].shape == (10, 768)
+    assert len(vectors[0]) == 768
